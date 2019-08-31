fa5e08dcf75d56955b9493f31b5fa7334de7d6e8
Kota Mori <kotam@Kota-no-MacBook-Air.local>
2019-08-31 22:01:30 +0900

    1st commit

---
+++ azuki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  azuki
+//
+//  Created by Kota Mori on 2019/08/31.
+//  Copyright © 2019 Kota Mori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
